48224ae860264976d1c54e8aa9ba3452d7ceb9a7
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-23 14:05:00 +0000

    onboarding screen data/model created

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const onboarding = [
+  {
+    id: 1,
+    title: "The perfect ride is just a tap away",
+    description: "The journey begins with you.",
+  },
+  {
+    id: 2,
+    title: "Ride with confidence and comfort",
+    description: "We prioritize your safety and convenience.",
+  },
+  {
+    id: 3,
+    title: "Your time, your ride, your way",
+    description: "Seamlessly book rides when you need them.",
+  },
+];
