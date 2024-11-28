e14ac9d09f056b5fd8c55d87c5d9c5177fb806bd
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-28 17:52:41 +0000

    bottom sheet for selecting ride added

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ConfirmRide = () => {
+  return (
+    <View>
+      <Text>Confirm Ride</Text>
+    </View>
+  );
+};
+
+export default ConfirmRide;
