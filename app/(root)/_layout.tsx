16c4ce2b2bdf029d8f5587982692211a09dc1d9a
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-28 19:30:50 +0000

    routes added

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -5,8 +5,8 @@ const Layout = () => {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="find-ride" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="confirm-ride" options={{ headerShown: false }} />
-      <Stack.Screen name="book-ride" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="confirm-ride" options={{ headerShown: false }} />
+      <Stack.Screen name="book-ride" options={{ headerShown: false }} />
     </Stack>
   );
 };
